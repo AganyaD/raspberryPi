9abf3ffb97f02f8c134dada7265bc143dca6d7df
aganya dejen <aganya.dejen@gmail.com>
2019-04-17 09:50:40 +0300

    iiiiissss

--- ConsoleApp2/ConsoleApp2/main.cs
+++ ConsoleApp2/ConsoleApp2/main.cs
@@ -345,8 +345,8 @@ namespace ConsoleApp2
             int duty = 10;
             bool demo = false;
             bool exsit = false;
-            printMessage("Open python process");
-            pythonProsses("sudo python /home/pi/Desktop/aganya/rpiApp/ConsoleApp2/ConsoleApp2/bin/Debug/pwm.py");
+            //printMessage("Open python process");
+            //pythonProsses("sudo python /home/pi/Desktop/aganya/rpiApp/ConsoleApp2/ConsoleApp2/bin/Debug/pwm.py");
 
 
             while (true)
